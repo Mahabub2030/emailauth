ddbf900d70454c9a2f5312e91194704874e71b16
mahabub <mahabubalom72091@gmail.com>
2024-04-12 22:22:00 +0300

    adding

--- src/Componeten/Register/Register.jsx
+++ src/Componeten/Register/Register.jsx
@@ -2,42 +2,52 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebaseConfig";
 import { useState } from "react";
 
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
 const Register = () => {
-        const [RegisterError , setRegisterError] = useState ('');
-        const [success ,setSuccess] = useState ('');
- 
-        const handelRegister = e =>{
+        const [RegisterError, setRegisterError] = useState('');
+        const [success, setSuccess] = useState('');
+
+        const [ShowPassword, SetShowPassword] = useState(false);
+
+        const handelRegister = e => {
                 e.preventDefault();
                 console.log('frome submoting');
                 const email = e.target.email.value;
                 const password = e.target.password.value;
+                const accepted = e.target.terms.checked;
 
-                console.log(email, password);
+                console.log(email, password ,accepted);
 
-                 // rest error
-                 setRegisterError('');
-                 setSuccess('');
+                // rest error
+                setRegisterError('');
+                setSuccess('');
 
-                if (password .length < 6){
-                        setRegisterError ('shoed be password 6 carcter');
+
+                if (password.length < 6) {
+                        setRegisterError('shoed be password 6 carcter');
                         return;
                 }
-               
+                else if(!accepted){
+                        setRegisterError('Please accept our terms and constion !')
+                        return;
+                }
+
 
                 // create user
 
                 createUserWithEmailAndPassword(auth, email, password)
-                .then(result => {
-                        console.log(result.user)
-                        setSuccess('user Creat done')
-                })
-                .catch(error => {
-                        console.error(error);
-                        setRegisterError(error.message);
-                })
-                
+                        .then(result => {
+                                console.log(result.user)
+                                setSuccess('user Creat done')
+                        })
+                        .catch(error => {
+                                console.error(error);
+                                setRegisterError(error.message);
+                        })
 
-                
+
+
         }
         return (
                 <div>
@@ -45,14 +55,32 @@ const Register = () => {
                                 <h2 className="text-3xl mb-4">Please regsister </h2>
 
                                 <form onSubmit={handelRegister}>
-                                        <input className="mb-4 w-3/4 py-2 px-4" type="email" name="email" id="" placeholder="Your-Email" required />
+                                        <input className="mb-4 w-full py-2 px-4" type="email" name="email" id="" placeholder="Your-Email" required />
                                         <br />
-                                        <input className="mb-4 w-3/4 py-2 px-4" type="password" name="password" id="" placeholder="Enter Your Password" required />
+                                        <div className=" mb-4 relative ">
+                                                <input className=" w-full py-2 px-4"
+                                                        type={ShowPassword ? "text" : "password"}
+                                                        name="password"
+                                                        id="" placeholder="Enter Your Password"
+                                                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+                                                <span className="absolute top-3 right-2 " onClick={() => SetShowPassword(!ShowPassword)}>
+                                                        {
+                                                                ShowPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                                                        }
+                                                </span>
+                                        </div>
                                         <br />
-                                        <input className="mb-4 w-3/4 btn btn-primary"  type="submit" value="Regstier" />
+                                        <div className="mb-4">
+                                        <input type="checkbox" name="terms" id="" />
+                                        <label className="ml-2 mb-4" htmlFor="">Accecpt for <a href="">terams codition</a></label>
+                                       
+                                        </div>
+                                        <br/>
+
+                                        <input className="mb-4 w-full btn btn-primary" type="submit" value="Regstier" />
                                 </form>
                                 {
-                                        RegisterError  && <p className="text-red-500">(RegisterError)</p>
+                                        RegisterError && <p className="text-red-500">(RegisterError)</p>
                                 }
                                 {
                                         success && <p className="text-green-500">(success the email)</p>
